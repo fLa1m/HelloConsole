652b04b7dcdcef9b7ecfaa2974b696fc3e5b6ea3
Andrew Mokin <parkovka.mokin@yandex.ru>
2023-01-26 19:01:06 +0300

    Example008 Two Friends And Dog

---
+++ Example008_TwoFriendsAndDog/Program.cs
@@ -0,0 +1,28 @@
+﻿int distance = 10000;
+int FirstFriendSpeed = 1;
+int SecondFriendSpeed = 2;
+int DogSpeed = 5;
+int friend = 2;
+int count = 0;
+int time = 0;
+
+while (distance > 10)
+{
+    if (friend == 1)
+    {
+        time = distance / (FirstFriendSpeed + DogSpeed);
+        friend = 2;
+    }
+
+    else
+    {
+        time = distance / (SecondFriendSpeed + DogSpeed);
+        friend = 1;
+    }
+    distance = distance - (FirstFriendSpeed + SecondFriendSpeed) * time;
+    count += 1;
+}
+
+Console.Write("Собака пробежала ");
+Console.Write(count);
+Console.Write(" раз.");
