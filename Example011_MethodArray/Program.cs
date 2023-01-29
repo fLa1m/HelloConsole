bcce37587702a07c8a980e6eee4f7e2f3c09dca2
Andrew Mokin <parkovka.mokin@yandex.ru>
2023-01-30 00:08:00 +0300

    Example011 MethodArray

---
+++ Example011_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 11, 22, 34, 64, 57, 36, 57, 98 };
+
+int n = array.Length;
+int find = 57;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
