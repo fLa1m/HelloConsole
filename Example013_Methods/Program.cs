94e5ad36f503973eb7cb210adc7e9ed42487979d
Andrew Mokin <parkovka.mokin@yandex.ru>
2023-02-05 14:59:37 +0300

    Lection 3

---
+++ Example013_Methods/Program.cs
@@ -0,0 +1,63 @@
+﻿// Вид 1
+void Method1()
+{
+    Console.WriteLine("Автор...");
+}
+Method1(); // вызов метода
+
+// Вид 2
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+Method2("Текст сообщения");
+
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+Method2(msg: "Текст", count: 4); // можно явно указывать имена аргументов, в таком случае их можно перечислять не по порядку
+Method2(count: 4, msg: "Новый текст");
+
+// Вид 3
+
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+
+int year = Method3();
+Console.WriteLine(year);
+
+// Вид4
+
+// string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = String.Empty;
+
+//     while (i < count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+// }
+
+string Method4(int count, string text)
+{
+    string result = String.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+
+string res = Method4(10, "asdf");
+Console.WriteLine(res);
