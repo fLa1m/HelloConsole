def3b1853a46d1177310394f246afb921c0f0311
Andrew Mokin <parkovka.mokin@yandex.ru>
2023-02-20 23:31:25 +0300

    Lection 7 end

--- Example021_Recursion/Program.cs
+++ Example021_Recursion/Program.cs
@@ -149,20 +149,50 @@
 //     System.Console.WriteLine(fi[i].Name);
 // }
 
-void CatalogInfo(string path, string indent = "")
+// void CatalogInfo(string path, string indent = "")
+// {
+//     DirectoryInfo catalog = new DirectoryInfo(path);
+//     DirectoryInfo[] catalogs = catalog.GetDirectories();
+//     for (int i = 0; i < catalogs.Length; i++)
+//     {
+//         Console.WriteLine($"{indent}{catalogs[i].Name}");
+//         CatalogInfo(catalogs[i].FullName, indent + " ");
+//     }
+//     FileInfo[] files = catalog.GetFiles();
+//     for (int i = 0; i < files.Length; i++)
+//     {
+//         Console.WriteLine($"{indent}{files[i].Name}");
+//     }
+// }
+// string path = "C:/C# Homework/HelloConsole";
+// CatalogInfo(path);
+
+/// Игра в пирамидки
+
+// void Towers(string with = "1", string on = "3", string some = "2", int count = 3)
+// {
+//     if (count > 1) Towers(with, some, on, count - 1);
+//     Console.WriteLine($"{with} >> {on}");
+//     if (count > 1) Towers(some, on, whith, count - 1);
+// }
+
+// Towers();
+
+/// Обход разных структур
+
+string emp = String.Empty;
+string[] tree = { emp, "/", "*", "10", "-", "+", emp, emp, "4", "2", "1", "3" };
+//                 0    1    2     3    4    5    6    7    8    9    10   11
+void InOrderTraversal(int pos = 1)
 {
-    DirectoryInfo catalog = new DirectoryInfo(path);
-    DirectoryInfo[] catalogs = catalog.GetDirectories();
-    for (int i = 0; i < catalogs.Length; i++)
+    if (pos < tree.Length)
     {
-        Console.WriteLine($"{indent}{catalogs[i].Name}");
-        CatalogInfo(catalogs[i].FullName, indent + " ");
-    }
-    FileInfo[] files = catalog.GetFiles();
-    for (int i = 0; i < files.Length; i++)
-    {
-        Console.WriteLine($"{indent}{files[i].Name}");
+        int left = 2 * pos;
+        int right = 2 * pos + 1;
+        if (left < tree.Length && !String.IsNullOrEmpty(tree[left])) InOrderTraversal(left);
+        Console.WriteLine(tree[pos]);
+        if (right < tree.Length && !String.IsNullOrEmpty(tree[right])) InOrderTraversal(right);
     }
 }
-string path = "C:/C# Homework/HelloConsole";
-CatalogInfo(path);
+
+InOrderTraversal();
