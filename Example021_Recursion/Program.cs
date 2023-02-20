e611d825c6058ccc657285884e4167549eb5883a
Andrey Mokin <parkovka.mokin@yandex.ru>
2023-02-20 22:08:10 +0300

    Lection 7 begining

---
+++ Example021_Recursion/Program.cs
@@ -0,0 +1,168 @@
+﻿/// Собрать строку с числами от a до b, a <= b
+
+// string NumberFor(int a, int b)
+// {
+//     string result = String.Empty;
+//     for (int i = a; i <= b; i++)
+//     {
+//         result += $"{i} ";
+//     }
+//     return result;
+// }
+
+// string NumberRec(int a, int b)
+// {
+//     if (a <= b) return $"{a} " + NumberRec(a + 1, b);
+//     else return String.Empty;
+// }
+
+// Console.WriteLine(NumberFor(1, 10));
+// Console.WriteLine(NumberRec(1, 10));
+
+// Собрать строку с числами от a до b, a >= b
+
+// string NumberFor(int a, int b)
+// {
+//     string result = String.Empty;
+//     for (int i = a; i >= b; i--)
+//     {
+//         result += $"{i} ";
+//     }
+//     return result;
+// }
+
+// string NumberRec(int a, int b)
+// {
+//     if (a <= b) return NumberRec(a + 1, b) + $"{a} ";
+//     else return String.Empty;
+// }
+
+// Console.WriteLine(NumberFor(10, 1));
+// Console.WriteLine(NumberRec(1, 10));
+
+/// Сумма чисел от 1 до n 
+
+// int SumFor(int n)
+// {
+//     int result = 0;
+//     for (int i = 1; i <= n; i++) result += i;
+//     return result;
+// }
+
+// int SumRec(int n)
+// {
+//     if (n == 0) return 0;
+//     else return n + SumRec(n - 1);
+// }
+
+// Console.WriteLine(SumFor(10));
+// Console.WriteLine(SumRec(10));
+
+/// Факториал числа
+
+// int FactorialFor(int n)
+// {
+//     int result = 1;
+//     for (int i = 1; i <= n; i++) result *= i;
+//     return result;
+// }
+
+// int FactorialRec(int n)
+// {
+//     if (n == 1) return 1;
+//     else return n * FactorialRec(n - 1);
+// }
+
+// Console.WriteLine(FactorialFor(10));
+// Console.WriteLine(FactorialRec(10));
+
+/// Вычислить a^n
+
+// int PowerFor(int a, int n)
+// {
+//     int result = 1;
+//     for (int i = 1; i <= n; i++) result *= a;
+//     return result;
+// }
+
+// int PowerRec(int a, int n)
+// { // return n == 0 ? 1 : PowerRec(a, n - 1) * a;
+//     if (n == 0) return 1;
+//     else return PowerRec(a, n - 1) * a;
+// }
+
+// int PowerRecMath(int a, int n)
+// {
+//     if (n == 0) return 1;
+//     else if (n % 2 == 0) return PowerRecMath(a * a, n / 2);
+//     else return PowerRecMath(a, n - 1) * a;
+// }
+
+// Console.WriteLine(PowerFor(2, 10));
+// Console.WriteLine(PowerRec(2, 10));
+// Console.WriteLine(PowerRecMath(2, 10));
+
+/// В нектором машинном алфавите имеются четыре буквы "а", "и", "с" и "в". Покажите все слова, состоящие 
+/// из Т букв, которые можно построить из букв этого алфавита.
+
+// char[] s = { 'а', 'и', 'с', 'в' };
+// int count = s.Length;
+// int n = 1;
+// for (int i = 0; i < count; i++)
+// {
+//     for (int j = 0; j < count; j++)
+//     {
+//         for (int k = 0; k < count; k++)
+//         {
+//             for (int l = 0; l < count; l++)
+//             {
+//                 Console.WriteLine($"{n++,-5}{s[i]}{s[j]}{s[k]}{s[l]}");
+//             }
+//         }
+//     }
+// }
+
+// int n = 1;
+// void FindWords(string alphabet, char[] word, int length = 0)
+// {
+//     if (length == word.Length)
+//     {
+//         Console.WriteLine($"{n++} {new String(word)}"); return;
+//     }
+//     for (int i = 0; i < alphabet.Length; i++)
+//     {
+//         word[length] = alphabet[i];
+//         FindWords(alphabet, word, length + 1);
+//     }
+// }
+
+// FindWords("аисв", new char[5]);
+
+/// Как посмотреть содержимое папки
+
+// string path = "C:/C# Homework/HelloConsole";
+// DirectoryInfo di = new DirectoryInfo(path);
+// System.Console.WriteLine(di.CreationTime);
+// FileInfo[] fi = di.GetFiles();
+// for (int i = 0; i < fi.Length; i++)
+// {
+//     System.Console.WriteLine(fi[i].Name);
+// }
+
+void CatalogInfo(string path, string indent = "")
+{
+    DirectoryInfo catalog = new DirectoryInfo(path);
+    DirectoryInfo[] catalogs = catalog.GetDirectories();
+    for (int i = 0; i < catalogs.Length; i++)
+    {
+        Console.WriteLine($"{indent}{catalogs[i].Name}");
+        CatalogInfo(catalogs[i].FullName, indent + " ");
+    }
+    FileInfo[] files = catalog.GetFiles();
+    for (int i = 0; i < files.Length; i++)
+    {
+        Console.WriteLine($"{indent}{files[i].Name}");
+    }
+}
+string path = "C:/C# Homework/HelloConsole";
+CatalogInfo(path);
